3a93dd416a415f5e82efc4f1519ad142c6924db7
unknown <reislavi28@gmail.com>
2024-11-26 22:04:32 -0300

    coisas de bd q o marcos mandou

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+import { verifyToken } from "./utils/verify-token.js"
+import { getName } from "./utils/get-name.js"
+import { logout } from "./utils/logout.js"
+
+const url = "./login.html"
+
+verifyToken(url)
+getName()
+logout()
+
 let currentSlide = 0;
 
 function showSlide(index) {
